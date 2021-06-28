cf431363bf9bf04c6cadbfdeca4a7cf4c3c4f10c
Andrew Masih <armasih@outlook.com>
2021-06-28 21:55:29 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+let i = 0;
+
+while (i < 10)  {
+
+  console.log("i is: " + i + " and that is less than 10");
+
+  i ++;
+
+};
+
+/* 
+A 'while loop' allows us to run code while the condition in the parentheses is TRUE, if it is not TRUE, it stops the iteration.
+
+*/
